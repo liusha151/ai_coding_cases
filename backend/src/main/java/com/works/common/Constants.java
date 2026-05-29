01956f79e912648033f1b9c90ad96e172cbcc5de
liusha151 <liusha151@users.noreply.github.com>
2026-05-30 06:39:34 +0800

    feat: add common utilities and exception handling

---
+++ backend/src/main/java/com/works/common/Constants.java
@@ -0,0 +1,8 @@
+package com.works.common;
+
+public class Constants {
+    public static final String ROLE_ADMIN = "admin";
+    public static final String ROLE_USER = "user";
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
